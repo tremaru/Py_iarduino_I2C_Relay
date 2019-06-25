d2ddc3e8cd67da28dd6763eac9b2813213321f50
drondistortion <cancelbutton@gmail.com>
2019-06-25 20:17:39 +0300

    basic files

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
 	name = 'pyiArduinoI2Crelay',
 	package = ['pyiArduinoI2Crelay'],
-	version = '1.0',
+	version = '1.2',
 	description = 'iarduino I2C Relay module',
 	author = 'iarduino.ru',
 	author_email = 'shop@iarduino.ru',
-	url = 'https://github.com/tremaru/Py_iarduino_I2C_Relay',
-	download_url = 'https://github.com/tremaru/Py_iarduino_I2C_Relay/tarball/1.0',
+	url = 'https://github.com/tremaru/pyiArduinoI2Crelay',
+	download_url = 'https://github.com/tremaru/pyiArduinoI2Crelay/tarball/1.0',
 	keywords = ['Hardware','I2C','Raspberry Pi','iarduino.ru', 'Relay','Solid State Relay', 'Power Mosfets'],
 	classifiers = [],
  )
