7255a4b4e8e01dbefd58e1767ed83762f5e1a671
drondistortion <cancelbutton@gmail.com>
2019-06-25 18:43:29 +0300

    yet another name update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	version = '1.0',
 	description = 'iarduino I2C Relay module',
 	author = 'iarduino.ru',
-	author_email = 'shop@iardiono.ru',
+	author_email = 'shop@iarduino.ru',
 	url = 'https://github.com/tremaru/Py_iarduino_I2C_Relay',
 	download_url = 'https://github.com/tremaru/Py_iarduino_I2C_Relay/tarball/1.0',
 	keywords = ['Hardware','I2C','Raspberry Pi','iarduino.ru', 'Relay','Solid State Relay', 'Power Mosfets'],
