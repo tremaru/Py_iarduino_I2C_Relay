d012c5402959a9bc4e9b37637eb1a41b9345823d
drondistortion <cancelbutton@gmail.com>
2019-07-05 17:40:44 +0100

    setup for sdist upload

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
-from Cython.Distutils import build_ext
+#from Cython.Build import cythonize
+#from Cython.Distutils import build_ext
 
 def readme():
 	with open('README.md') as readme:
 		return readme.read()
 
 setup(name='pyiArduinoI2Crelay',
-	version='1.6.4.dev9',
+	version='1.6.5',
 	description='iarduino.ru module for Raspberry Pi',
 	long_description=readme(),
 	classifiers=[
@@ -25,9 +25,4 @@ setup(name='pyiArduinoI2Crelay',
 	include_package_data=True,
         zip_safe=False,
 	python_requires='>=3',
-        setup_requires=['Cython'],
-        install_requires=['Cython'],
-	cmdclass = {
-		"build_ext": build_ext
-	}
 )
