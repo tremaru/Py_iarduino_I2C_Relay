99688e2819948462a111ce457309b82a45675588
drondistortion <cancelbutton@gmail.com>
2019-08-02 23:18:49 +0300

    setup update

--- pyiArduinoI2Crelay/setup.py
+++ pyiArduinoI2Crelay/setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-  ext_modules = cythonize("pyiArduinoI2Cexpander.pyx")
+  ext_modules = cythonize("pyiArduinoI2Crelay.pyx")
 )
