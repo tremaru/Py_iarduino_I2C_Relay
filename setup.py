03c377dbe6b0a9bb3fd8877073b06aa8de377607
drondistortion <cancelbutton@gmail.com>
2019-08-06 13:43:57 +0300

    setup update

--- setup.py
+++ setup.py
@@ -2,25 +2,25 @@ from distutils.core import setup
 from distutils.extension import Extension
 
 def readme():
-	with open('README.md') as readme:
-		return readme.read()
+    with open('README.md') as readme:
+        return readme.read()
 
 setup(name='pyiArduinoI2Crelay',
-	version='1.6.8',
-	description='iarduino.ru module for Raspberry Pi',
-	long_description=readme(),
-	classifiers=[
-		'Programming Language :: Python :: 3',
-	],
-	url='http://github.com/tremaru/pyiArduinoI2Crelay',
-	author='iarduino.ru',
-	author_email='shop@iarduino.ru',
-	license='MIT',
-	package=['pyiArduinoI2Crelay'],
-	ext_modules = [Extension(
-		name="pyiArduinoI2Crelay",
-		sources=["pyiArduinoI2Crelay/pyiArduinoI2Crelay.cpp"])],
-	include_package_data=True,
-        zip_safe=False,
-	python_requires='>=3',
+    version='1.6.9',
+    description='iarduino.ru module for Raspberry Pi',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    url='http://github.com/tremaru/pyiArduinoI2Crelay',
+    author='iarduino.ru',
+    author_email='shop@iarduino.ru',
+    license='MIT',
+    package=['pyiArduinoI2Crelay'],
+    ext_modules = [Extension(
+        name="pyiArduinoI2Crelay",
+        sources=["pyiArduinoI2Crelay/pyiArduinoI2Crelay.cpp"])],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3',
 )
